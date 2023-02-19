716bd76bc8fa9981911198244c336ed7fbaeca79
Simon de Cock <saj.decock@student.avans.nl>
2023-02-19 13:28:30 +0100

    start work on week 2

--- Week2/001.Moon/src/Moon.java
+++ Week2/001.Moon/src/Moon.java
@@ -16,10 +16,9 @@ public class Moon extends Application {
     private ResizableCanvas canvas;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
-        canvas = new ResizableCanvas(g -> draw(g), mainPane);
+        canvas = new ResizableCanvas(this::draw, mainPane);
         mainPane.setCenter(canvas);
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("Moon");
@@ -27,18 +26,41 @@ public class Moon extends Application {
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
     }
 
-
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-    }
+        graphics.translate(canvas.getWidth()/2, canvas.getHeight()/2);
+        graphics.scale( 1, -1);
 
+        GeneralPath moon = new GeneralPath();
+        moon.moveTo(0, 200);
+        moon.curveTo(
+                100, 170,
+                100, 30,
+                0, 0
+        );
+
+        GeneralPath moonSubtract = new GeneralPath();
+        moonSubtract.moveTo(0, 200);
+        moonSubtract.curveTo(
+                50, 170,
+                50, 30,
+                0, 0
+        );
+
+        Area moonArea = new Area(moon);
+        Area moonSubtractArea = new Area(moonSubtract);
+        moonArea.subtract(moonSubtractArea);
+
+        graphics.setColor(Color.yellow);
+        graphics.fill(moonArea);
+        graphics.setColor(Color.black);
+        graphics.draw(moonArea);
+    }
 
     public static void main(String[] args)
     {
         launch(Moon.class);
     }
-
 }
--- Week2/002.YingYang/src/YingYang.java
+++ Week2/002.YingYang/src/YingYang.java
@@ -8,6 +8,7 @@ import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
@@ -16,8 +17,7 @@ public class YingYang extends Application {
     private ResizableCanvas canvas;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -27,14 +27,54 @@ public class YingYang extends Application {
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
     }
 
-
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-    }
+        graphics.translate(canvas.getWidth()/2, canvas.getHeight()/2);
+        graphics.scale( 1, -1);
 
+        GeneralPath circle = new GeneralPath();
+        circle.moveTo(0, 150);
+        circle.curveTo(
+                -200, 150,
+                -200, -150,
+                0, -150
+        );
+        circle.curveTo(
+                200, -150,
+                200, 150,
+                0, 150
+        );
+
+        GeneralPath upperPath = new GeneralPath();
+        upperPath.moveTo(0, 150);
+        upperPath.curveTo(
+                80,130,
+                80,20,
+                0, 0
+        );
+        upperPath.curveTo(
+                -80, -20,
+                -80, -130,
+                0, -150
+        );
+        upperPath.curveTo(
+                -200, -150,
+                -200, 150,
+                0, 150
+        );
+
+        graphics.setColor(Color.black);
+        graphics.draw(circle);
+        graphics.draw(upperPath);
+        Area circleArea = new Area(circle);
+        circleArea.subtract(new Area(upperPath));
+        graphics.fill(circleArea);
+        graphics.fill(new Ellipse2D.Double(0, 65, 20, 20));
+        graphics.setColor(Color.white);
+        graphics.fill(new Ellipse2D.Double(0, -85, 20, 20));
+    }
 
     public static void main(String[] args)
     {
