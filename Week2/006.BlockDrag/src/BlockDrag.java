b8f693639a38415fb2d8bf6b16fb1ad5768e5cfb
simonavans <saj.decock@student.avans.nl>
2023-02-25 23:00:26 +0100

    Start work on BlockDrag

--- Week2/006.BlockDrag/src/BlockDrag.java
+++ Week2/006.BlockDrag/src/BlockDrag.java
@@ -8,37 +8,47 @@ import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
 
 public class BlockDrag extends Application {
-    ResizableCanvas canvas;
+    private ResizableCanvas canvas;
+    private Point2D startPan = new Point2D.Double();
+    private double zoomFactor = 1.05;
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
+        canvas.resize(960, 540);
         mainPane.setCenter(canvas);
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("Block Dragging");
         primaryStage.show();
+        canvas.requestFocus();
+
+        FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
+        graphics.setTransform(new AffineTransform());
 
         canvas.setOnMousePressed(e -> mousePressed(e));
         canvas.setOnMouseReleased(e -> mouseReleased(e));
-        canvas.setOnMouseDragged(e -> mouseDragged(e));
+        canvas.setOnMouseDragged(e -> mouseDragged(e, graphics));
+        canvas.setOnScroll(e -> scroll(e, graphics));
 
-        draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+        draw(graphics);
     }
 
 
     public void draw(FXGraphics2D graphics)
     {
-        graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        graphics.fillRect(300, 300, 50, 50);
     }
 
 
@@ -49,6 +59,7 @@ public class BlockDrag extends Application {
 
     private void mousePressed(MouseEvent e)
     {
+        startPan.setLocation(e.getX(), e.getY());
     }
 
     private void mouseReleased(MouseEvent e)
@@ -56,8 +67,26 @@ public class BlockDrag extends Application {
 
     }
 
-    private void mouseDragged(MouseEvent e)
+    private void mouseDragged(MouseEvent e, FXGraphics2D graphics)
     {
+        double diffX = e.getX() - startPan.getX();
+        double diffY = e.getY() - startPan.getY();
+
+        startPan.setLocation(e.getX(), e.getY());
+        graphics.translate(diffX / zoomFactor, diffY / zoomFactor);
+        draw(graphics);
+    }
+
+    private void scroll(ScrollEvent e, FXGraphics2D graphics) {
+        Point2D beforeScroll = new Point2D.Double(e.getX(), e.getY());
+        double localZoomFactor = e.getDeltaY() < 0 ? 0.95 : zoomFactor;
+
+        graphics.scale(canvas.getScaleX() * localZoomFactor, canvas.getScaleY() * localZoomFactor);
+        draw(graphics);
+
+        Point2D diffScroll = new Point2D.Double(e.getX() - beforeScroll.getX(), e.getY() - beforeScroll.getY());
+        graphics.translate(diffScroll.getX(), diffScroll.getY());
+        draw(graphics);
     }
 
 }
